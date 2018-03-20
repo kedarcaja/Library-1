fa834b54fe666d859f51d6871f1b225a45f539f0
vitekd55 <vicadvorak@seznam.cz>
2018-03-20 22:27:57 +0100

    ArrowTrap

---
+++ Puzzles/Assets/Scripts/Arrow.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Arrow : MonoBehaviour {
+
+
+  
+
+
+    public float targetTime = 10.0f;
+    public float vel;
+    private Vector2 veloc;
+    public GameObject arrow;
+    private GameObject projectile;
+   
+    void Start()
+    {
+
+      projectile = Instantiate(arrow, arrow.transform.position,arrow.transform.rotation) as GameObject;
+        veloc = (new Vector2(0, -vel));
+
+    }
+
+
+    void Update()
+    {
+
+
+      
+
+    }
+
+    private void FixedUpdate()
+    {
+
+
+
+        if (projectile != null)
+            projectile.GetComponent<Rigidbody2D>().velocity = veloc;
+        else
+            projectile = Instantiate(arrow, arrow.transform.position, arrow.transform.rotation) as GameObject;
+
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+    }
+
+}
